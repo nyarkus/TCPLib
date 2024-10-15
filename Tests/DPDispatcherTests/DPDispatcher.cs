39631ea22d114573f32a9156aa0e3c9758bd7205
nyarkus_ <klimenkoe2008@gmail.com>
2024-10-15 16:43:01 +0300

    Server tests

--- Tests/DPDispatcherTests/DPDispatcher.cs
+++ Tests/DPDispatcherTests/DPDispatcher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using TCPLib.Classes;
 using TCPLib.Client;
 using TCPLib.Client.DPDispatcher;
-using TCPLib.Server.SaveFiles;
+using TCPLib.Net.DPDispatcher;
 
 namespace Tests
 {
@@ -14,10 +14,18 @@ namespace Tests
     public class DPDispatcher
     {
         internal bool Received = false;
+        internal bool ServerReceived = false;
         internal Task OnReceived(DataPackageSource package)
         {
             if(package.Type == "Message")
                 Received = true;
+
+            return Task.CompletedTask;
+        }
+        internal Task OnServerReceived(DataPackageSource package)
+        {
+            if (package.Type == "Message")
+                ServerReceived = true;
 
             return Task.CompletedTask;
         }
@@ -43,6 +51,29 @@ namespace Tests
             await sclient.SendAsync(new Message() { Data = "meow" });
             await Task.Delay(1000);
             Assert.True(Received);
+
+            dispatcher.Stop();
+        }
+        [Fact]
+        public async Task DPDispatcherServer()
+        {
+            TCPLib.Client.Client client = new();
+            NetTest.StartServer();
+
+            var sserver = await client.Connect(System.Net.IPAddress.Parse("127.0.0.1"), NetTest.port);
+            if (TCPLib.Server.Net.Client.clients.Count == 0)
+                Assert.Fail("The client was unable to connect");
+
+            string t = sserver.client.Client.LocalEndPoint!.ToString()!.Split(':')[4];
+            var sclient = TCPLib.Server.Net.Client.clients.First(x => x.client.Client.RemoteEndPoint!.ToString()!.Split(':')[1] == t);
+
+            var handler = TCPLib.Server.DPDispatcher.DPHandlerRegistry.Create(DPFilter.Equals("Message"), new DataPackageReceive(OnServerReceived));
+            var dispatcher = new TCPLib.Server.DPDispatcher.DPDispatcherBuilder(sclient, handler).Build();
+            _ = Task.Run(dispatcher.Start);
+
+            await sserver.SendAsync(new Message() { Data = "meow" });
+            await Task.Delay(1000);
+            Assert.True(ServerReceived);
 
             dispatcher.Stop();
         }
