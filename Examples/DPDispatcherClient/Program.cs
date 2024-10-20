3fa8790e3c36d902d79a5cd614c207f9d78d75cf
nyarkus_ <klimenkoe2008@gmail.com>
2024-10-20 11:55:20 +0300

    documentation

--- Examples/DPDispatcherClient/Program.cs
+++ Examples/DPDispatcherClient/Program.cs
@@ -28,15 +28,11 @@ namespace DPDispatcherClient
 
             Console.WriteLine("Write something");
             var input = Console.ReadLine();
-            if(input == null)
-            {
-                dispatcher.Stop();
-                await server.Disconnect();
-                return;
-            }
+
             await server.SendAsync(new DataPackageSource("msg", Encoding.UTF8.GetBytes(input)));
 
-            // Use of TimeProvider is optional and is synonymous with DateTimeOffster.UtcNow;
+
+            // Use of TimeProvider is optional and is synonymous with DateTimeOffset.UtcNow;
             SendTime = TCPLib.Time.TimeProvider.Now;
             while (Running)
             {
